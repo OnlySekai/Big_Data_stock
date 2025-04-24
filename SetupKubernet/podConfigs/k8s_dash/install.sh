52e053873e2e48151bb3aeb33ef90d7c5c157395
OnlySekai <minhtuan25902@gmail.com>
2025-04-24 17:50:27 +0700

    :+1: k8s dash

---
+++ SetupKubernet/podConfigs/k8s_dash/install.sh
@@ -0,0 +1,6 @@
+# Add kubernetes-dashboard repository
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
